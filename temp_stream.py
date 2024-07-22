68fe7a634fa31486b1f6c2349b49f0714d9afee1
Qha3 <93575028+Qha3@users.noreply.github.com>
2024-07-22 14:59:49 +0900

    Create House collision

---
+++ temp_stream.py
@@ -0,0 +1,29 @@
+import pygame
+from pathlib import Path
+from enum import Enum, auto
+
+CURRENT_DIR = Path(__file__).parent
+
+
+
+# DIC_NPC = {' ': pygame.image.load()}
+
+# class CollisionEnum(Enum) :
+
+#     ENTERABLE = (auto(), ' ')
+
+#     def __init__(self, id, symbol: str) :
+#         self.id = id
+#         self.symbol = symbol
+
+
+
+def load_text_file(relative_path_name: str) -> list[list[str]] :
+    """  テキストファイルから、1文字の`str`の2次元`listを生成`  """
+
+    with open(Path(CURRENT_DIR, relative_path_name)) as file :
+        temp_list = file.readlines()
+        return [[temp_list[row][col] for col in range(40)] for row in range(22)].copy()
+    
+
+print(load_text_file("house.txt"))
